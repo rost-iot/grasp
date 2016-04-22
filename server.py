dec1de275372b682f34a56892eb32fc37692e432
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-04-23 01:27:03 +0200

    added format_command functionality to server

--- server.py
+++ server.py
@@ -1,21 +1,30 @@
 from flask import Flask, request, render_template
 import os
-from match_commands import match_commands
 import json
+from match_commands import match_commands
+from watson.call_watson import call_watson
 
 app = Flask(__name__)
 
 @app.route("/api/match_commands", methods=["POST"])
 def match_commands_api():
-        form = request.form
-        response = "error connecting. Lorentz is a shitty programmer"
-        if 'commands' and 'text' in form:
-            commands = json.loads(form.get('commands'))
-            text = form.get('text')
-            print(text)
-            print(commands)
-            response = match_commands(text, commands)
-        return json.dumps(response)
+    form = request.form
+    response = "error connecting. Lorentz is a shitty programmer"
+    if 'commands' and 'text' in form:
+        commands = json.loads(form.get('commands'))
+        text = form.get('text')
+        response = match_commands(text, commands)
+    return json.dumps(response)
+
+@app.route('/api/format_command', methods=['POST'])
+def format_command():
+    form = request.form
+    response = "error connecting. Lorentz is a shitty programmer"
+    if 'text' in form:
+        text = form.get('text')
+        command = call_watson(text)[0]
+        response = command
+    return json.dumps(response)
 
 port = int(os.getenv("VCAP_APP_PORT", "5000"))
 host = os.getenv("VCAP_APP_HOST", "localhost")
