491a74b1f0e2b4e62d6c10aff6f967c58d4abb1c
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-04-23 01:14:45 +0200

    made match_commands.py runnable from command line

--- match_commands.py
+++ match_commands.py
@@ -1,5 +1,7 @@
 from watson import text_to_trees
 from get_distance import get_distance
+import sys
+import json
 
 def match_commands(text, known_commands):
     entered_command = text_to_trees(text)[0]
@@ -7,3 +9,11 @@ def match_commands(text, known_commands):
         distance = get_distance(entered_command, command['command'])
         command['distance'] = distance
     return known_commands 
+
+if __name__ == '__main__':
+    args = sys.argv
+    text = args[1]
+    known_commands = json.loads(args[2])
+    results = match_commands(text, known_commands)
+    print(results)
+    sys.stdout.flush()
