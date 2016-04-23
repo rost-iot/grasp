ed6855a3407b213731e07920739b65344d0b2920
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-04-23 13:13:40 +0200

    changed attribute name from command to signature in the input for match_commands

--- match_commands.py
+++ match_commands.py
@@ -6,7 +6,7 @@ import json
 def match_commands(text, known_commands):
     entered_command = text_to_trees(text)[0]
     for command in known_commands:
-        distance = get_distance(entered_command, command['command'])
+        distance = get_distance(entered_command, command['signature'])
         command['distance'] = distance
     return known_commands 
 
